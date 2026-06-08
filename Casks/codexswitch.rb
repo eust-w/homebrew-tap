850f09e15c5b14050def4c543e67b7a1119b9fb7
eust-w <eustanceuwu@gmail.com>
2026-06-08 21:06:07 +0800

    Update codexswitch cask package

--- Casks/codexswitch.rb
+++ Casks/codexswitch.rb
@@ -1,6 +1,6 @@
 cask "codexswitch" do
   version "0.1.0"
-  sha256 "d89331f1f7f61fd609973f824b531760833427662f7a54c96c4bd6fca2cc64ef"
+  sha256 "23d3e2850ce8c334271a3f43642f93dad21ec884c01fc1f67a4e2c138a72f210"
 
   url "https://github.com/eust-w/homebrew-tap/releases/download/codexswitch-preview-#{version}/codexswitch_#{version}_arm64.dmg"
   name "codexswitch"
@@ -8,7 +8,7 @@ cask "codexswitch" do
   homepage "https://github.com/eust-w/homebrew-tap"
 
   depends_on arch: :arm64
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "codexswitch.app"
 
